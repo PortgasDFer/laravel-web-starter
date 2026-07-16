b0bc6d05efab9f45a14db151e66f24764dd731a6
PortgasDFer <fernando_ls96hotmail.com>
2026-07-16 13:46:45 -0600

    Inicio

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.nerox')
 @section('content')
- <!-- tp-minimal__area start -->
+<!-- tp-minimal__area start -->
 <section class="tp-minimal__area position-relative">
     <div class="tp-minimal__design-thumb" data-background="{{ asset('images/hero.jpg') }}"></div>
         <div class="container">
@@ -27,26 +27,183 @@
 </section>
 <!-- tp-minimal__area end -->
 
- <!-- tp-hero-area start -->
-<section class="tp-minimal__area position-relative">
-    <div class="tp-minimal__design-thumb tp-minimal__design-thumb-2" data-background="assets/img/about/5/ab-img-5.jpg"></div>
+<!-- tp-about-area start -->
+<section  class="tpfrn-about-area pt-135 pb-110 p-relative">
     <div class="container">
-        <div class="row justify-content-end">
-            <div class="col-xl-6 col-lg-6 col-md-6">
-                <div class="tp-minimal__design tp-minimal__design-2">
-                <div class="tp-minimal__design-wrapper tp-minimal__design-wrapper-2">
-                    <h3 class="tpds-title mb-50">We're motivated by  <br> the <span> desire to achieve. </span></h3>
-                    <p class="tpds-text-2 mb-50">There are many variations of passages of Lorem Ipsum available, but <br> the majority have suffered alteration.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. </p>
+        <div class="row align-content-center">
+            <div class="col-xl-5">
+            <div class="tpfrn-about">
+                <h4 class="tpfrn-about-title mb-30">Creamos tecnología que impulsa negocios.</h4>
+                <p>En AAF Desarrollo Digital diseñamos, desarrollamos e implementamos soluciones tecnológicas para empresas que buscan optimizar procesos, fortalecer su presencia digital y crecer con herramientas creadas a la medida.
+                </p>
+                <p>Desde sitios web corporativos hasta plataformas empresariales, sistemas internos y aplicaciones, trabajamos con un enfoque en rendimiento, experiencia de usuario y escalabilidad.
+                </p>
+            </div>
+            </div>
+            <div class="col-xl-7">
+            <div class="tpfrn-about-image s-about-img">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img src="assets/img/about/about-4.jpg" class="ab-iamge mb-30" alt="about-img">
+                    </div>
+                    <div class="col-sm-6">
+                        <img src="assets/img/about/about-3.jpg" class="ab-iamge mb-30" alt="about-img">
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- tp-about-area end -->
 
-                    <div class="tp-minimal__button mt-55">
-                        <a href="about-me.html" class="tp-solid-btn">Download Cv</a>
+<!-- tpfrn-services-area start -->
+<section class="tpfrn-services-area">
+    <div class="container">
+        <div class="tpfrn-services-wrapper pt-100 pb-85">
+            <div class="row">
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                <div class="tpservices tpservices-4 mb-30">
+                    <div class="tpservices__icon mb-22">
+                        <i class="flaticon-coding"></i>
+                    </div>
+                    <div class="tpservices__content">
+                        <h4 class="tpservices__title mb-25"><a href="services-details.html">Desarrollo Web</a></h4>
+                        <p>Sitios rápidos, seguros y optimizados para buscadores.</p>
+                        <div class="tpservices__btn mt-30">
+                        <a href="services-details.html">
+                            <i class="flaticon-right-arrow-1"></i>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                <div class="tpservices tpservices-4 mb-30">
+                    <div class="tpservices__icon mb-30">
+                        <i class="flaticon-curve"></i>
+                    </div>
+                    <div class="tpservices__content">
+                        <h4 class="tpservices__title mb-25"><a href="services-details.html">Software empresarial</a></h4>
+                        <p>Soluciones personalizadas para optimizar procesos y mejorar la eficiencia operativa.</p>
+                        <div class="tpservices__btn mt-30">
+                        <a href="services-details.html">
+                            <i class="flaticon-right-arrow-1"></i>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                <div class="tpservices tpservices-4 mb-30">
+                    <div class="tpservices__icon mb-30">
+                        <i class="flaticon-branding-2"></i>
+                    </div>
+                    <div class="tpservices__content">
+                        <h4 class="tpservices__title mb-25"><a href="services-details.html">Branding Digital</a></h4>
+                        <p> Identidad visual, experiencia de usuario y presencia profesional.</p>
+                        <div class="tpservices__btn mt-30">
+                        <a href="services-details.html">
+                            <i class="flaticon-right-arrow-1"></i>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                <div class="tpservices tpservices-4 mb-30">
+                    <div class="tpservices__icon mb-30">
+                        <i class="flaticon-tools"></i>
+                    </div>
+                    <div class="tpservices__content">
+                        <h4 class="tpservices__title mb-25"><a href="services-details.html">Consultoría Tecnológica</a></h4>
+                        <p>Te ayudamos a elegir la mejor solución para tu empresa.</p>
+                        <div class="tpservices__btn mt-30">
+                        <a href="services-details.html">
+                            <i class="flaticon-right-arrow-1"></i>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- tpfrn-services-area end -->
+
+
+<!-- tpbs-project-area start -->
+<div class="tpbs-project-area pt-120 pb-80">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="tpbs-section-wrapper text-center mb-30">
+                <span class="tpbs-sub-title mb-15">Proyectos recientes.</span>
+                <h3 class="tpbs-title">Soluciones que<span> ya están generando resultados.</span></h3>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-30">
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="tpbs-project mb-50">
+                <div class="tpbs-project__thumb mb-30">
+                    <div class="tpbs-project__thumb-image">
+                        <a href="project-details.html"><img src="assets/img/project/4/project-1.jpg" alt="project-img"></a>
+                    </div>
+                    <div class="tpbs-project__thumb-shape">
+                        <img src="assets/img/project/4/tpbs-shape.png" alt="">
+                    </div>
+                </div>
+                <div class="tpbs-project__content">
+                    <span class="tpbs-project__category mb-10">WEBSITE DESIGN</span>
+                    <h4 class="tpbs-project__title"><a href="project-details.html">Ecommerce Product Landing</a></h4>
+                    <div class="tpbs-project-btn mt-20">
+                        <a href="project-details.html" class="tpbs-btn-2">More Details <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="tpbs-project mb-50">
+                <div class="tpbs-project__thumb mb-30">
+                    <div class="tpbs-project__thumb-image">
+                        <a href="project-details.html"><img src="assets/img/project/4/project-2.jpg" alt="project-img"></a>
+                    </div>
+                    <div class="tpbs-project__thumb-shape">
+                        <img src="assets/img/project/4/tpbs-shape.png" alt="">
+                    </div>
+                </div>
+                <div class="tpbs-project__content">
+                    <span class="tpbs-project__category mb-10">WEBSITE Develop</span>
+                    <h4 class="tpbs-project__title"><a href="project-details.html">Logo Design is creativity</a></h4>
+                    <div class="tpbs-project-btn mt-20">
+                        <a href="project-details.html" class="tpbs-btn-2">More Details <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="tpbs-project mb-50">
+                <div class="tpbs-project__thumb mb-30">
+                    <div class="tpbs-project__thumb-image">
+                        <a href="project-details.html"><img src="assets/img/project/4/project-3.jpg" alt="project-img"></a>
+                    </div>
+                    <div class="tpbs-project__thumb-shape">
+                        <img src="assets/img/project/4/tpbs-shape.png" alt="">
+                    </div>
+                </div>
+                <div class="tpbs-project__content">
+                    <span class="tpbs-project__category mb-10">UX/UI DESIGN</span>
+                    <h4 class="tpbs-project__title"><a href="project-details.html">Design is a creative part</a></h4>
+                    <div class="tpbs-project-btn mt-20">
+                        <a href="project-details.html" class="tpbs-btn-2">More Details <i class="fa-solid fa-arrow-right"></i></a>
                     </div>
                 </div>
                 </div>
             </div>
         </div>
     </div>
-</section>
-    <!-- tp-hero-area end -->
+</div>
+<!-- tpbs-project-area end -->
 @endsection
